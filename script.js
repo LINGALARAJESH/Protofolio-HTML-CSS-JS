c298769859b347e88418e68d6945b96cc2e2c41c
LINGALARAJESH <rajeshlingala08@gmail.com>
2024-04-01 10:36:39 +0530

    lastcomiit030424

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+// ....................................................google sheet.............................//
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwUbjy273SwvglLs03GDav7C8akmkTZbnJ3xMwEJ-pqWMECNX6oa4gl_EyicRHujRtK/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg=document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+           msg.innerHTML="Thank You!!"
+            setTimeout(function(){msg.innerHTML=''},5000)
+           form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
